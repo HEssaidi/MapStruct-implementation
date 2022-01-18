fb92aa70bd684cd5329eec567a37fcf54ce5585a
HEssaidi <essaidihajar50@gmail.com>
2022-01-18 09:59:39 +0100

    add MapperImpl manually

---
+++ src/main/java/com/example/mapstruct/mapper/EmployeeMapperConverter.java
@@ -0,0 +1,41 @@
+package com.example.mapstruct.mapper;
+
+import com.example.mapstruct.dto.EmployeeDto;
+import com.example.mapstruct.models.Employee;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeMapperConverter implements EmployeeMapper {
+
+    @Override
+    public EmployeeDto toDto(Employee emp) {
+        if ( emp == null ) {
+            return null;
+        }
+
+        EmployeeDto employeeDto = new EmployeeDto();
+
+        employeeDto.setFullName( emp.getFirstName() );
+        if ( emp.getId() != null ) {
+            employeeDto.setId( emp.getId().intValue() );
+        }
+        employeeDto.setJobRole( emp.getJobRole() );
+
+        return employeeDto;
+    }
+
+    @Override
+    public Employee fromDto(EmployeeDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Employee.EmployeeBuilder employee = Employee.builder();
+
+        employee.firstName( dto.getFullName() );
+        employee.id( (long) dto.getId() );
+        employee.jobRole( dto.getJobRole() );
+
+        return employee.build();
+    }
+}
--- src/main/java/com/example/mapstruct/resource/EmployeeResource.java
+++ src/main/java/com/example/mapstruct/resource/EmployeeResource.java
@@ -1,8 +1,7 @@
 package com.example.mapstruct.resource;
 
 import com.example.mapstruct.dto.EmployeeDto;
-import com.example.mapstruct.mapper.EmployeeMapper;
-import com.example.mapstruct.mapper.EmployeeMapperImpl;
+import com.example.mapstruct.mapper.EmployeeMapperConverter;
 import com.example.mapstruct.models.Employee;
 import com.example.mapstruct.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +17,11 @@ public class EmployeeResource {
     @Autowired
     private EmployeeService employeeService;
     @Autowired
-    private EmployeeMapperImpl employeeMapper;
+    private EmployeeMapperConverter employeeMapperConverter;
 
     @PostMapping("/")
     public Employee saveEmpl(@RequestBody EmployeeDto employeeDto){
-        Employee employee = employeeMapper.fromDto(employeeDto);
+        Employee employee = employeeMapperConverter.fromDto(employeeDto);
         return employeeService.saveEmployee(employee);
     }
 }
